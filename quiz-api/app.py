7b90ff8258001d48490cc798644b2385e0f57b88
hzmagm <59263934+hzmagm@users.noreply.github.com>
2023-04-15 00:21:36 +0200

    database connection

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,18 +1,33 @@
 import json
-import sqlite3
+import sqlite3,os
 import jwt,hashlib
-from flask import Flask, request
+from flask import Flask, jsonify, request,g
 from flask_cors import CORS
-
 from jwt_utils import build_token
+
 
 app = Flask(__name__)
 CORS(app)
 
-def get_db_connection():
-    conn = sqlite3.connect('QuizePro.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+DATABASE = 'D:/quiz-app/quiz-api/QuizePro.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 
 @app.route('/')
 def hello_world():
@@ -37,5 +52,14 @@ def Auth():
     else:
         return 'Unauthorized', 401
     
+@app.route('/questions/<int:question_id>', methods=['GET'])
+def getQuestById(question_id):
+    question = query_db('SELECT * from questions join possibleAnswers where possibleAnswers.question_Id==?',
+                [question_id], one=True)
+    print('connected')
+    if question is None:
+        return jsonify({'error': 'Question not found'}), 404
+    return jsonify(question)
+
 if __name__ == "__main__":
     app.run()
